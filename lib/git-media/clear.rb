6c246295ff2bcd5b3e633a916947953393296429
Aron Granberg <aron.granberg@gmail.com>
2014-10-20 00:26:29 +0200

    Tiny output changes tweaks

--- lib/git-media/clear.rb
+++ lib/git-media/clear.rb
@@ -9,12 +9,12 @@ module GitMedia
     end
     
     def self.clear_local_cache
-      # find files in media buffer and upload them
+      # find files in media buffer and delete all pushed files
       all_cache = Dir.chdir(GitMedia.get_media_buffer) { Dir.glob('*') }
       unpushed_files = @push.get_unpushed(all_cache)
       pushed_files = all_cache - unpushed_files
       pushed_files.each do |sha|
-        puts "removing " + sha[0, 8]
+        puts "Removing " + sha[0, 8]
         File.unlink(File.join(GitMedia.get_media_buffer, sha))
       end
     end
